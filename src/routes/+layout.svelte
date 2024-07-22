b8b3ae3af8172e313fbbd9d7e6b0e9769bf9e63b
Nikonell <rozhkovn08@gmail.com>
2024-07-23 03:54:07 +0700

    feat: basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import "$lib/assets/css/fonts.css";
+    import "$lib/assets/css/globals.css";
+</script>
+
+<svelte:head>
+    <title>Team Durt</title>
+    <meta name="description" content="We are a team of content creators for Minecraft.">
+</svelte:head>
+
+<slot />
