cd433443cce0f8e412812deab303ca548423ee53
Nikonell <rozhkovn08@gmail.com>
2024-08-23 17:11:23 +0700

    feat: translations

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -5,6 +5,13 @@ const defaultLocale = "en"
 
 register("en", () => import("./locales/en.json"))
 register("ru", () => import("./locales/ru.json"))
+register("be", () => import("./locales/ru.json"))
+register("kk", () => import("./locales/ru.json"))
+register("uk", () => import("./locales/uk.json"))
+register("de", () => import("./locales/de.json"))
+register("fr", () => import("./locales/fr.json"))
+register("it", () => import("./locales/it.json"))
+register("es", () => import("./locales/es.json"))
 
 init({
     fallbackLocale: defaultLocale,
